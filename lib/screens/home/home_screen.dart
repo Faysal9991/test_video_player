fb6422830dd6a456cd5eda6c5aeb78433e98116a
Faysal9991 <faysaltanvir9991@gmail.com>
2023-12-05 15:56:26 +0600

    home screen desin done locical work done

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test_2/component/custom_space.dart';
 import 'package:flutter_test_2/provider/video_provider.dart';
+import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -56,7 +57,11 @@ class _HomeScreenState extends State<HomeScreen> {
                         style: Theme.of(context).textTheme.headlineSmall,
                       ),
                       gap(hight: 16),
-                      ListView.builder(
+                      ListView.separated(
+                        padding:EdgeInsets.zero ,
+                        separatorBuilder: (context,index){
+                          return gap(hight: 10);
+                        },
                           physics: const NeverScrollableScrollPhysics(),
                           shrinkWrap: true,
                           itemCount: provider.videoList.length + 1,
@@ -78,15 +83,86 @@ class _HomeScreenState extends State<HomeScreen> {
                                 color: Colors.white,
                                 child: Column(
                                   children: [
-                                    Container(
-                                      height: 200,
-                                      width: double.infinity,
-                                      decoration: BoxDecoration(
-                                          image: DecorationImage(
-                                              image: NetworkImage(provider
-                                                  .videoList[index]
-                                                  .thumbnail!))),
-                                    )
+                                    Stack(
+                                      children: [
+                                        Container(
+                                          height: 200,
+                                          width: double.infinity,
+                                          decoration: BoxDecoration(
+                                              image: DecorationImage(
+                                                  image: NetworkImage(provider
+                                                      .videoList[index]
+                                                      .thumbnail!))),
+                                        ),
+                                        Positioned(
+                                            bottom: 20.0,
+                                            right: 20.0,
+                                            child: Container(
+                                              decoration: BoxDecoration(
+                                                  color: Colors.black,
+                                                  borderRadius:
+                                                      BorderRadius.circular(5)),
+                                              child: Padding(
+                                                padding:
+                                                    const EdgeInsets.all(4.0),
+                                                child: Text(
+                                                  "${provider.videoList[index].duration}",
+                                                  style: Theme.of(context)
+                                                      .textTheme
+                                                      .bodySmall!
+                                                      .copyWith(
+                                                          color: Colors.white),
+                                                ),
+                                              ),
+                                            ))
+                                      ],
+                                    ),
+                                    gap(hight: 16),
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment.start,
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Expanded(
+
+                                          child: Container(
+                                            height: 40,
+                                            width: 40,
+                                            decoration:  BoxDecoration(
+                                                shape: BoxShape.circle,
+                                                image: DecorationImage(
+                                                    image: NetworkImage(provider
+                                                        .videoList[index]
+                                                        .channelImage!)),
+                                                color: Colors.green),
+
+                                          ),
+                                        ),
+                                        Expanded(
+                                          flex:4,
+                                          child: Column(
+                                            mainAxisAlignment: MainAxisAlignment.start,
+                                            crossAxisAlignment: CrossAxisAlignment.start,
+                                            children: [
+                                              Text("${provider.videoList[index].title}"),
+                                              Row(
+                                                mainAxisAlignment: MainAxisAlignment.start,
+                                                children: [
+                                                  Text("${provider.videoList[index].viewers} views",style: const TextStyle(color: Colors.grey),),
+                                                  gap(width: 20),
+                                                  Text(DateFormat('MMM d, y').format(provider.videoList[index].dateAndTime!),style: const TextStyle(color: Colors.grey)),
+                                                ],
+                                              )
+                                            ],
+                                          ),
+                                        ),
+                                        Expanded(
+                                            child: IconButton(onPressed: (){
+
+                                            }, icon: const Icon(Icons.more_vert,color: Colors.grey)))
+
+                                      ],
+                                    ),
+                                    gap(hight: 16),
                                   ],
                                 ),
                               );
